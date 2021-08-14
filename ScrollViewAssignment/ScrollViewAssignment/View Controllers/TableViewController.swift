a1c26fa066e19db09514cfafe16d2bdea9de4569
Artem <Stozhok>
2021-08-15 01:59:40 +0300

    done assignment without CoreData

--- ScrollViewAssignment/ScrollViewAssignment/View Controllers/TableViewController.swift
+++ ScrollViewAssignment/ScrollViewAssignment/View Controllers/TableViewController.swift
@@ -9,28 +9,52 @@ import UIKit
 
 class TableViewController: UITableViewController {
     
+    @IBOutlet weak var addButton: UIBarButtonItem!
+    
+    var choosingItems: [ShopItem] = []
+    
     var shoppingItems = [ShopItem(name: "Glasses", description: "This is the best Glasses \n I'he ever seen", nameOfimage: "Glasses"),
                          ShopItem(name: "Dessert", description: "This is so yummy", nameOfimage: "Dessert"),
-                         ShopItem(name: "Camera Lens", description: "I wish I had this \ncamera lens", nameOfimage: "Camera lens")]
+                         ShopItem(name: "Camera Lens", description: "I wish I had this \ncamera lens", nameOfimage: "Camera lens"),
+                         ShopItem(name: "Phone", description: "Leader of industry", nameOfimage: "Phone"),
+                         ShopItem(name: "Pan", description: "Cook your best dish", nameOfimage: "Pan"),
+                         ShopItem(name: "Pen", description: "For best writers", nameOfimage: "Pen"),
+                         ShopItem(name: "Strawberry", description: "Delicious!!!", nameOfimage: "Strawberry"),
+                         ShopItem(name: "Ball", description: "If you love football", nameOfimage: "Ball"),
+                         ShopItem(name: "Skateboard", description: "Better than BMX", nameOfimage: "Skateboard")]
+    
+    @IBAction func addItem(_ sender: UIBarButtonItem) {
+        guard shoppingItems.count > 0 else {
+            return
+        }
+        let newRowIndex = choosingItems.count
+        choosingItems.append(shoppingItems.remove(at: shoppingItems.count.random))
+        let indexPath = IndexPath(row: newRowIndex, section: 0)
+        let indexPaths = [indexPath]
+        tableView.insertRows(at: indexPaths, with: .automatic)
+    }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return shoppingItems.count
+        return choosingItems.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "rowOfItem", for: indexPath)
         if let customCell = cell as? CustomTableViewCell {
-            let item = shoppingItems[indexPath.row]
+            let item = choosingItems[indexPath.row]
             customCell.setupCell(with: item)
             customCell.completion = { [weak self] isPlusAction in
+                guard (self != nil) else {
+                    return
+                }
                 if isPlusAction {
-                    self?.shoppingItems[indexPath.row].quantity += 1
-                        customCell.setupCell(with: (self?.shoppingItems[indexPath.row])!)
+                    self?.choosingItems[indexPath.row].quantity += 1
+                        customCell.setupCell(with: (self?.choosingItems[indexPath.row])!)
                 } else {
-                    if (self?.shoppingItems[indexPath.row].quantity)! > 0 {
-                        self?.shoppingItems[indexPath.row].quantity -= 1
+                    if (self?.choosingItems[indexPath.row].quantity)! > 0 {
+                        self?.choosingItems[indexPath.row].quantity -= 1
                     }
-                    customCell.setupCell(with: (self?.shoppingItems[indexPath.row])!)
+                    customCell.setupCell(with: (self?.choosingItems[indexPath.row])!)
                 }
             }
         }
@@ -42,11 +66,23 @@ class TableViewController: UITableViewController {
             if let cvc = segue.destination as? ShowCellViewController {
                 if let cell = sender as? CustomTableViewCell {
                     if let index = tableView.indexPath(for: cell){
-                        let item = shoppingItems[index.row]
+                        let item = choosingItems[index.row]
                         cvc.shopItem = item
                     }
                 }
             }
+        }
+    }
+}
+
+extension Int {
+    var random: Int {
+        if self > 0 {
+            return Int(arc4random_uniform(UInt32(self)))
+        } else if self < 0 {
+            return -Int(arc4random_uniform(UInt32(abs(self))))
+        } else {
+            return 0
         }
     }
 }
